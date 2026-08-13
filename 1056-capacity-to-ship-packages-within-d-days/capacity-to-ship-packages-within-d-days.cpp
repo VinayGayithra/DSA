5c2d84a957f0c01ed56c1125623955ed2db3fd0f
VINAY T <mrvinayvini@gmail.com>
2026-08-13 10:49:32 +0530

    Time: 18 ms (11.54%) | Memory: 35.1 MB (20.26%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
+    bool possible(vector<int>& weights, int days, int cap, int i, int day, int sum) {
+        if (i == weights.size())
+            return day <= days;
+
+        if (sum + weights[i] <= cap)
+            return possible(weights, days, cap, i + 1, day, sum + weights[i]);
+
+        if (day == days)
+            return false;
+
+        return possible(weights, days, cap, i + 1, day + 1, weights[i]);
+    }
+
+    int bs(vector<int>& weights, int days, int low, int high) {
+        if (low > high)
+            return low;
+
+        int mid = low + (high - low) / 2;
+
+        if (possible(weights, days, mid, 0, 1, 0))
+            return bs(weights, days, low, mid - 1);
+
+        return bs(weights, days, mid + 1, high);
+    }
+
     int shipWithinDays(vector<int>& weights, int days) {
         int low = *max_element(weights.begin(), weights.end());
         int high = accumulate(weights.begin(), weights.end(), 0);
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            int day = 1, sum = 0;
-
-            for (int w : weights) {
-                if (sum + w > mid) {
-                    day++;
-                    sum = w;
-                } else {
-                    sum += w;
-                }
-            }
-
-            if (day <= days)
-                high = mid - 1;
-            else
-                low = mid + 1;
-        }
-
-        return low;
+        return bs(weights, days, low, high);
     }
 };
