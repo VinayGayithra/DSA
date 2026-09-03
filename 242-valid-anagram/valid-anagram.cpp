6b7e854d4d78d56a7a13bfb2105f145f739468da
VINAY T <mrvinayvini@gmail.com>
2026-09-03 15:18:40 +0530

    Time: 4 ms (33.95%) | Memory: 9.6 MB (92.32%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,12 +1,29 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         sort(s.begin(),s.end());
+//         sort(t.begin(),t.end());
+//         if(s.size()!=t.size()) return false;
+//         if(s == t){
+//             return true;
+//         }
+//         return false;  
+//     }
+// };
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s.size()!=t.size()) return false;
-        if(s == t){
-            return true;
+        if(s.size() != t.size())
+            return false;
+        map<char,int> mp;
+        for(char ch : s)
+            mp[ch]++;
+        for(char ch : t)
+            mp[ch]--;
+        for(auto it : mp) {
+            if(it.second != 0)
+                return false;
         }
-        return false;  
+        return true;
     }
 };
