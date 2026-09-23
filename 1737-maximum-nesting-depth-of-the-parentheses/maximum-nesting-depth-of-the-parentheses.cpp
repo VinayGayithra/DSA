3857b29781d14dbd5434bdd70740608905b4d9bc
VINAY T <mrvinayvini@gmail.com>
2026-09-23 21:12:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (7.75%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,16 +1,32 @@
+// class Solution {
+// public:
+//     int maxDepth(string s) {
+//         int open=0;
+//         int res=0;
+//         for(char ch:s){
+//             if(ch=='('){
+//                 open=open+1;
+//                 if(open>res)
+//                     res=open;
+//             }else if(ch==')'){
+//                 open=open-1;
+//             }
+//         }
+//         return res;
+//     }
+// };
 class Solution {
 public:
     int maxDepth(string s) {
-        int open=0;
+        stack<char> st;
         int res=0;
         for(char ch:s){
             if(ch=='('){
-                open=open+1;
-                if(open>res)
-                    res=open;
+                st.push(ch);
             }else if(ch==')'){
-                open=open-1;
+                st.pop();
             }
+            res=max(res,(int)st.size());
         }
         return res;
     }
